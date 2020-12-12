9b0ecf682fd40ac6a05cb3b4ea774404a9f86dec
2046619@student.uma.pt <2046619@student.uma.pt>
2020-12-12 19:25:21 +0000

    commit

---
+++ src/main/java/BaseDeDadosHospitalarDeDoencasInfectocontagiosas/Administracao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDeDadosHospitalarDeDoencasInfectocontagiosas;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class Administracao {
+    //variaveis de instancia
+    //construtor
+    public Administracao()
+    {
+        
+    }
+    //metodos
+    //getters e setters
+    //toString
+    //equals
+}
