27b568de43329ea799ee1538e67d91903e224c7a
JoaoCupido <61253001+JoaoCupido@users.noreply.github.com>
2020-12-12 18:26:27 +0000

    new commit

---
+++ src/main/java/BaseDeDadosHospitalarDeDoencasInfectocontagiosas/Paciente.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDeDadosHospitalarDeDoencasInfectocontagiosas;
+
+/**
+ *
+ * @author jcupi
+ */
+public class Paciente {
+    private int anonascimento;
+    private String id;
+    
+    private static int contarPacientes;
+    
+    public Paciente(int anonascimento){
+        this.anonascimento = anonascimento;
+        if((this.id).equals("")) {
+            contarPacientes++;
+            id = "ID" + contarPacientes;
+        }
+    }
+}
